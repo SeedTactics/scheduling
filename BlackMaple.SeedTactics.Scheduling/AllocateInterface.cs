cefb1d175b8df8bdd02b44d703bf5e1735fe1aa3
John Lenz <none@none>
2017-10-07 11:12:10 -0500

    initial scheduling plugin api

---
+++ BlackMaple.SeedTactics.Scheduling/AllocateInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using BlackMaple.MachineWatchInterface;
+
+namespace BlackMaple.SeedTactics.Scheduling
+{
+    public class AllocateResult
+    {
+        public string ScheduleId { get; set; }
+        public List<JobPlan> Jobs { get; set; }
+        public List<SimulatedStationUtilization> SimStations { get; set; }
+        public List<string> NewScheduledOrders { get; set; }
+        public List<SeedOrders.ScheduledPartWithoutBooking> NewExtraParts { get; set; }
+    }
+
+    public struct FlexPlan
+    {
+        public Newtonsoft.Json.Linq.JObject FlexJson { get; set; }
+        public object MastModel { get; set; }
+    }
+
+    public enum BookingFillMethod
+    {
+        FillInAnyOrder,
+        FillOnlyByDueDate
+    }
+
+    public interface IAllocateInterface
+    {
+        AllocateResult Allocate(
+            SeedOrders.UnscheduledStatus bookings,
+            JobsAndExtraParts previousSchedule,
+            FlexPlan flexPlan,
+            DateTime startLocal,
+            DateTime endLocal,
+            BookingFillMethod fillMethod,
+            string scheduleId);
+    }
+}
