fbbd44973fbb88b03124842b7e65920196bda558
John Lenz <wuzzeb@gmail.com>
2017-11-18 15:07:51 -0600

    Fix version prefix script

--- version-prefix.py
+++ version-prefix.py
@@ -3,8 +3,8 @@ import subprocess
 # Script similar to GitVersion, but simplier and focused on our use case.
 
 curtag = subprocess.check_output(["hg", "id", "-t", "-r", ".^"])
-if curtag == "":
-    print (curtag)
+if curtag.strip() != "":
+    print (curtag.strip().decode("utf-8"))
 else:
     lasttag = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag()"])
     parts = lasttag.decode('utf-8').rstrip().split(".")
