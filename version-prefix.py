a1f9625180cb09c0b2a74380fa3d7f440451941f
John Lenz <wuzzeb@gmail.com>
2017-11-18 13:15:58 -0600

    Don't use shell in version-prefix python script

--- version-prefix.py
+++ version-prefix.py
@@ -1,10 +1,10 @@
 import subprocess
 
-curtag = subprocess.check_output("hg id -t -r '.^'", shell=True)
+curtag = subprocess.check_output(["hg", "id", "-t", "-r", ".^"])
 if curtag == "":
     print (curtag)
 else:
-    lasttag = subprocess.check_output("hg id -t -r 'ancestors(.) and tag()'", shell=True)
+    lasttag = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag()"])
     parts = lasttag.decode('utf-8').rstrip().split(".")
     patch = int(parts[2]) + 1
     print (parts[0] + "." + parts[1] + "." + str(patch))
