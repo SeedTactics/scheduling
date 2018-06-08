d62cd59a4bf030d8d832641ad3e722a2932a34fa
John Lenz <wuzzeb@gmail.com>
2018-06-08 10:24:52 -0500

    Update to fms insight api 1.2

--- allocatecli/Program.cs
+++ allocatecli/Program.cs
@@ -60,7 +60,7 @@ namespace AllocateCli
       //load inputs
 
       var jsonSettings = new JsonSerializerSettings();
-      jsonSettings.Converters.Add(new TimespanConverter());
+      jsonSettings.Converters.Add(new BlackMaple.FMSInsight.API.TimespanConverter());
       jsonSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
       jsonSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
 
@@ -173,36 +173,6 @@ namespace AllocateCli
 
         return Assembly.Load(assemblyName);
       }
-    }
-  }
-
-  public class TimespanConverter : JsonConverter
-  {
-    public override bool CanConvert(Type objectType)
-    {
-      return objectType == typeof(TimeSpan);
-    }
-
-    public override bool CanRead => true;
-    public override bool CanWrite => true;
-
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-      if (objectType != typeof(TimeSpan))
-        throw new ArgumentException();
-
-      var spanString = reader.Value as string;
-      if (spanString == null)
-        return null;
-      if (TimeSpan.TryParse(spanString, out TimeSpan result))
-        return result;
-      return System.Xml.XmlConvert.ToTimeSpan(spanString);
-    }
-
-    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    {
-      var duration = (TimeSpan)value;
-      writer.WriteValue(System.Xml.XmlConvert.ToString(duration));
     }
   }
 }
